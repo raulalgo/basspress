710f823300e8fd9d8172e21b7140a1972407777b
Raúl Álvarez González <raulalgo@obturecode.com>
2015-11-28 13:01:52 +0000

    Got rid of some ghost code by commenting wp_head. I think I should be getting it back but don´t know how for now

--- header.php
+++ header.php
@@ -9,7 +9,8 @@
  * @since Twenty Fifteen 1.0
  */
  ?><!DOCTYPE html>
- <head>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
@@ -18,7 +19,7 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
-	<?php wp_head(); ?>
+	<?php //wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -34,7 +35,7 @@
 				<?php endif;
 			?>
 		<?php else: ?>
-			<div class="bg-orange absolute top-0 left-0 right-0 z1" style="min-height:480px;z-index:-1">hola</div>
+			<div class="bg-blue absolute top-0 left-0 right-0 z1" style="min-height:480px;z-index:-1"></div>
 			<div class="flex py2 px3 navy z2">
 				<h1 class="h3 bold caps" style="letter-spacing:0"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 		<?php endif; ?>
