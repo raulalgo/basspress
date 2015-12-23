0b222decbf5da672ec25d58fb1e1b77c5dc85ab1
Raúl Álvarez González <raulalgo@obturecode.com>
2015-12-23 16:03:18 +0100

    He puesto todo el tema en los mismos verdes que hemos usado en redes sociales

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 	</div><!-- .site-content -->
 
-	<footer id="" class="fit bg-navy" role="contentinfo">
+	<footer id="" class="fit bg-deuvegreen" role="contentinfo">
 		<div class="p2 white h5">
 			<?php
 				/**
@@ -23,7 +23,7 @@
 				do_action( 'twentyfifteen_credits' );
 			?>
 			<!--<a class="white" href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Deuvede es un podcast de Carlos Álvarez (<a href=@husky) y Raúl Álvarez (@raulalgo)', 'basspress' ), 'WordPress' ); ?></a>-->
-			<p class="mb0">Deuvedé es un Podcast de Carlos Álvarez (<a href="http://twitter.com/husky" class="white underline">@husky</a>) y Raúl Álvarez (<a href="http://twitter.com/raulalgo" class="white underline">@raulalgo</a>)</p>
+			<p class="mb0">Deuvedé (<a href="http://twitter.com/deuvede_fm" class="white underline">@deuvede_fm</a>)es un Podcast de Carlos Álvarez (<a href="http://twitter.com/husky" class="white underline">@husky</a>) y Raúl Álvarez (<a href="http://twitter.com/raulalgo" class="white underline">@raulalgo</a>)</p>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
 
